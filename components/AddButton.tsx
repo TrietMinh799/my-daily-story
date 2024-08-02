2aed096f35e9230c266e00a8b9c5c5808b9e3770
Trietminh <luckyblockblack@gmail.com>
2024-08-02 22:39:47 +0700

    Moditify little styling

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function AddButton() {
   return (
     <Link
-      className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border"
+      className="py-2 px-3 flex rounded-md no-underline border-black hover:bg-slate-600 border"
       href="/add"
     >
       Thêm Câu Chuyện Mới
