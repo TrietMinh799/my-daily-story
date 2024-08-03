85eb70757974b55793f0c7cf096ebd840539ccf2
Trietminh <luckyblockblack@gmail.com>
2024-08-03 11:47:40 +0700

    Change to client component to make sure that it will refresh

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,35 @@
+"use client"
 import Article from "@/components/Article";
-import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-export default async function Index() {
+export default function Index() {
 
   const supabase = createClient()
+  const [_data, _setData] = useState([{}])
 
-  const { data, error } = await supabase.from("posts").select("*")
+  async function getData() {
+    const { data, error } = await supabase.from("posts").select("*")
+
+    _setData(data!)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-black h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <AuthButton />
+          <Link className="btn btn-active btn-neutral" href="/login">Login</Link>
         </div>
       </nav>
 
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
-          {data && data.map((post, index) => {
+          {_data && _data.map((post: any, index) => {
             return (
               <Article title={post.title} id={post.id} key={index} />
             )
