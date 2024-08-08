194f7f6277253491dec72625d3cc56fa88a48547
Trietminh <luckyblockblack@gmail.com>
2024-08-08 16:56:21 +0700

    Something

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -20,7 +20,7 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hi,<Link href={"/profile"}>
+      Hi, <Link href={"/profile"}>
         <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
           <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
         </div>
