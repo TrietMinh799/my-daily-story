e175949bd5f68168421f3c25a3ec79afc285de06
Trietminh <luckyblockblack@gmail.com>
2024-08-08 16:56:44 +0700

    Fix signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -11,6 +11,7 @@ export default function SignUp() {
         "use server";
 
         const origin = headers().get("origin");
+        const username = formData.get("username") as string;
         const email = formData.get("email") as string;
         const password = formData.get("password") as string;
         const supabase = createClient();
@@ -19,22 +20,26 @@ export default function SignUp() {
             email,
             password,
             options: {
+                data: {
+                    userName: username
+                },
                 emailRedirectTo: `${origin}/auth/callback`,
             },
         });
 
+        console.log(error)
 
         if (error) {
-            return redirect("/login?message=Could not authenticate user");
+            return redirect("/signup");
         }
 
-        return redirect("/login?message=Check email to continue sign in process");
+        return redirect("/signup?message=Check email to continue sign in process");
     };
 
 
 
     return (
-        <form className="bg-gray-50 dark:bg-gray-900">
+        <div className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <Link href="/signup" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     Sign up
@@ -46,16 +51,20 @@ export default function SignUp() {
                         </h1>
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
+                                <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your name..." required />
+                            </div>
+                            <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" />
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required />
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
+                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
                             <SubmitButton formAction={signUp} pendingText="Sign up...">
                                 Create an account
@@ -67,6 +76,6 @@ export default function SignUp() {
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
     )
 }
