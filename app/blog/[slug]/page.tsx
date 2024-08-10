261d93075fe6b8dd2cf84e6820325304be6bee5c
Trietminh <luckyblockblack@gmail.com>
2024-08-10 15:47:15 +0700

    Change loading screen

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -38,7 +38,14 @@ export default function Index({ params }: { params: { slug: string } }) {
     }, [])
 
     if (!loading) return (
-        <><div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" /></>
+        <>
+            <div className='flex space-x-2 justify-center items-center bg-white h-screen dark:invert'>
+                <span className='sr-only'>Loading...</span>
+                <div className='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+                <div className='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+                <div className='h-8 w-8 bg-black rounded-full animate-bounce'></div>
+            </div>
+        </>
     )
 
 
