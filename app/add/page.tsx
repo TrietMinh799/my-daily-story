5efa83225194148ed11cb9fe32d92e750e5f87bc
Trietminh <luckyblockblack@gmail.com>
2024-08-13 22:19:37 +0700

    Change styling

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -6,6 +6,9 @@ import { useRouter } from 'next/navigation';
 import Editor from '@/components/editor/advanced-editor';
 import { JSONContent } from 'novel';
 import "../prosemirror.css";
+import { Box, Container } from '@radix-ui/themes';
+import { Button, Description, Field, Input, Label } from '@headlessui/react';
+import clsx from 'clsx';
 
 export default function Add() {
     const supabase = createClient()
@@ -42,19 +45,34 @@ export default function Add() {
         router.push('/')
     }
     return (
-        <div className='m-4'>
-            <form action="#">
-                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" onClick={handleClick}>Add a new story</button>
-                <input
-                    onChange={(e) => {
-                        setTitle(e.target.value)
-                    }}
-                    type="text"
-                    placeholder="Story name"
-                    className="input input-bordered w-full max-w-xs"
-                    required />
-                <Editor onChange={setContent} />
-            </form>
-        </div>
+        <Box style={{}}>
+            <Container size="2">
+                <form action="#">
+                    <Box py="3">
+                        <Button className='inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white' onClick={handleClick}>Add a new story</Button>
+                    </Box>
+                    <Box py="3">
+                        <Field>
+                            <Input
+                                onChange={(e) => {
+                                    setTitle(e.target.value)
+                                }}
+                                className={clsx(
+                                    'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
+                                    'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                                )}
+                                type="text"
+                                placeholder="Story name..."
+                                data-invalid
+                                required />
+                        </Field>
+
+                    </Box>
+                    <Box p="3" maxWidth="800px" maxHeight="500px">
+                        <Editor onChange={setContent} />
+                    </Box>
+                </form>
+            </Container>
+        </Box>
     )
 }
