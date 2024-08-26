6e8cc2737b03dfeca64163ca192882f6ed6e51be
Trietminh <luckyblockblack@gmail.com>
2024-08-26 21:14:35 +0700

    Change button

--- components/Article.tsx
+++ components/Article.tsx
@@ -9,6 +9,7 @@ import { User } from "@supabase/supabase-js";
 import Image from "next/image";
 import StarterKit from "@tiptap/starter-kit";
 import { Box, Card, Heading, Inset } from "@radix-ui/themes";
+import GetStartedButton from "./gettingstarted-button";
 
 export default function Article({ user_id, id, title, content }: any) {
 
@@ -59,12 +60,7 @@ export default function Article({ user_id, id, title, content }: any) {
                     </Link>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     </p>
-                    <Link href={`/blog/${id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                        </svg>
-                    </Link>
+                    <GetStartedButton text={"Read more"} path={`/blog/${id}`} />
                 </div>
             </Card>
         </Box>
